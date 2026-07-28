828d8c2e534dfc44e411cea5d172fda58160198d
Sepehr Abdous <sabdous1@jh.edu>
2026-07-28 12:56:08 -0400

    update

--- Omnet_Sims/dc_simulations/simulations/sims/run_sample.sh
+++ Omnet_Sims/dc_simulations/simulations/sims/run_sample.sh
@@ -15,8 +15,8 @@ rm -rf results
 bash dir_creator.sh
 
 # Ring
-echo "\n\n-------------------------------------------"
-echo "Running Ring"
+echo -e "\n\n-------------------------------------------"
+echo -e "Running Ring"
 opp_runall -j50 ../../src/dc_simulations -m -u Cmdenv -c ring -n ..:../../src:../../../inet/src:../../../inet/examples:../../../inet/tutorials:../../../inet/showcases --image-path=../../../inet/images -l ../../../inet/src/INET omnetpp_bcast_sample.ini
 do_extract ring_bcast
 mkdir logs/ring_bcast_sample
@@ -25,8 +25,8 @@ cp results/*.out logs/ring_bcast_sample/
 
 
 # Binary tree
-echo "\n\n-------------------------------------------"
-echo "Running Binary Tree"
+echo -e "\n\n-------------------------------------------"
+echo -e "Running Binary Tree"
 opp_runall -j50 ../../src/dc_simulations -m -u Cmdenv -c binary_tree -n ..:../../src:../../../inet/src:../../../inet/examples:../../../inet/tutorials:../../../inet/showcases --image-path=../../../inet/images -l ../../../inet/src/INET omnetpp_bcast_sample.ini
 do_extract tree_bcast
 mkdir logs/tree_bcast_sample
@@ -35,8 +35,8 @@ cp results/*.out logs/tree_bcast_sample/
 
 
 # Optimal
-echo "\n\n-------------------------------------------"
-echo "Running Optimal"
+echo -e "\n\n-------------------------------------------"
+echo -e "Running Optimal"
 opp_runall -j50 ../../src/dc_simulations -m -u Cmdenv -c optimal -n ..:../../src:../../../inet/src:../../../inet/examples:../../../inet/tutorials:../../../inet/showcases --image-path=../../../inet/images -l ../../../inet/src/INET omnetpp_bcast_sample.ini
 do_extract mcast_optimal_bcast
 mkdir logs/mcast_optimal_sample
@@ -45,8 +45,8 @@ cp results/*.out logs/optimal_sample/
 
 
 # Orca
-echo "\n\n-------------------------------------------"
-echo "Running Orca"
+echo -e "\n\n-------------------------------------------"
+echo -e "Running Orca"
 opp_runall -j50 ../../src/dc_simulations -m -u Cmdenv -c orca -n ..:../../src:../../../inet/src:../../../inet/examples:../../../inet/tutorials:../../../inet/showcases --image-path=../../../inet/images -l ../../../inet/src/INET omnetpp_bcast_sample.ini
 do_extract mcast_bcast_orca
 mkdir logs/orca_sample
@@ -55,8 +55,8 @@ cp results/*.out logs/orca_sample/
 
 
 # Elmo
-echo "\n\n-------------------------------------------"
-echo "Running Elmo"
+echo -e "\n\n-------------------------------------------"
+echo -e "Running Elmo"
 opp_runall -j50 ../../src/dc_simulations -m -u Cmdenv -c elmo -n ..:../../src:../../../inet/src:../../../inet/examples:../../../inet/tutorials:../../../inet/showcases --image-path=../../../inet/images -l ../../../inet/src/INET omnetpp_bcast_sample.ini
 do_extract mcast_bcast_elmo
 mkdir logs/elmo_sample
@@ -65,8 +65,8 @@ cp results/*.out logs/elmo_sample/
 
 
 # Peel
-echo "\n\n-------------------------------------------"
-echo "Running Peel"
+echo -e "\n\n-------------------------------------------"
+echo -e "Running Peel"
 opp_runall -j50 ../../src/dc_simulations -m -u Cmdenv -c peel -n ..:../../src:../../../inet/src:../../../inet/examples:../../../inet/tutorials:../../../inet/showcases --image-path=../../../inet/images -l ../../../inet/src/INET omnetpp_bcast_sample.ini
 do_extract mcast_bcast_peel
 mkdir logs/peel_sample
