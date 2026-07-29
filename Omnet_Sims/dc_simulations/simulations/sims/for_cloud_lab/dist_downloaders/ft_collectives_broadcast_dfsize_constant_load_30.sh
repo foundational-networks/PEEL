32523e5c8beed4aff2929ef9b822bc0695447744
Sepehr Abdous <sabdous1@jh.edu>
2026-07-29 15:18:32 -0400

    remove for_cloud_lab

--- Omnet_Sims/dc_simulations/simulations/sims/for_cloud_lab/dist_downloaders/ft_collectives_broadcast_dfsize_constant_load_30.sh
+++
@@ -1,7 +0,0 @@
-sudo mkdir /mnt/data/OneDrive
-sudo mkdir /mnt/data/OneDrive/mburst_simulations
-sudo mkdir /mnt/data/OneDrive/mburst_simulations/forwarding_dbs
-sudo mkdir /mnt/data/OneDrive/mburst_simulations/forwarding_dbs/distributions
-sudo git clone https://sepehrabdous96:raGV9hCmJXrQCzHaZ4EX@bitbucket.org/scalable-mcast/ft_collectives_broadcast_dfsize_constant_load_30.git /mnt/data/OneDrive/mburst_simulations/forwarding_dbs/distributions/
-sudo rm -rf /mnt/data/OneDrive/mburst_simulations/forwarding_dbs/distributions/.git*
-sudo bzip2 -d /mnt/data/OneDrive/mburst_simulations/forwarding_dbs/distributions/*.bz2
--- Omnet_Sims/dc_simulations/simulations/sims/for_cloud_lab/dist_downloaders/ls_collectives_broadcast_1pergpu_dscale_constant_load_50.sh
+++
@@ -1,7 +0,0 @@
-sudo mkdir /mnt/data/OneDrive
-sudo mkdir /mnt/data/OneDrive/mburst_simulations
-sudo mkdir /mnt/data/OneDrive/mburst_simulations/forwarding_dbs
-sudo mkdir /mnt/data/OneDrive/mburst_simulations/forwarding_dbs/distributions
-sudo git clone https://sepehrabdous96:raGV9hCmJXrQCzHaZ4EX@bitbucket.org/scalable-mcast/ls_collectives_broadcast_1pergpu_dscale_constant_load_50.git /mnt/data/OneDrive/mburst_simulations/forwarding_dbs/distributions/
-sudo rm -rf /mnt/data/OneDrive/mburst_simulations/forwarding_dbs/distributions/.git*
-sudo bzip2 -d /mnt/data/OneDrive/mburst_simulations/forwarding_dbs/distributions/*.bz2
