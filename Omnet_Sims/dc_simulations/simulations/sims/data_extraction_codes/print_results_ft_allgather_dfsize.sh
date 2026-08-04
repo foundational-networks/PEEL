6c32c3389e2758cea077ba9d8b1b144f61245c73
Sepehr Abdous <sabdous1@jh.edu>
2026-08-04 13:33:25 -0400

    update

--- Omnet_Sims/dc_simulations/simulations/sims/data_extraction_codes/print_results_ft_allgather_dfsize.sh
+++ Omnet_Sims/dc_simulations/simulations/sims/data_extraction_codes/print_results_ft_allgather_dfsize.sh
@@ -4,5 +4,6 @@ echo -e "\n\nPrinting results into archive/ft_allgather_dfsize.txt\n"
 
 cp VARIABLES_ft_allgather_dfsize.py VARIABLES.py
 python3 response_time_constant_load.py > archive/ft_allgather_dfsize.txt
+python plotter_linechart.py --input_file_path archive/ft_allgather_dfsize.txt --output_fig_path figs/ft_allgather_dfsize --cases 2 8 32 128 512 --x_axis_title "Message Size (MB)"
 
 echo "Done!\n\n"
